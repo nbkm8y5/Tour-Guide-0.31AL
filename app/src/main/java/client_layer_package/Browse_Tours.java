f3f85a02307e7e2ac7b69324d2018040c2d9af5b
adrianbureu <bureuadrian@gmail.com>
2016-07-15 21:30:51 -0400

    Update Browse_Tours.java

--- app/src/main/java/client_layer_package/Browse_Tours.java
+++ app/src/main/java/client_layer_package/Browse_Tours.java
@@ -3,5 +3,26 @@ package client_layer_package;
 /**
  * Created by Ale on 7/10/2016.
  */
-public class Browse_Tours {
+import java.util.ArrayList;
+
+
+public class BrowseTours {
+
+    private ArrayList[] listViews;
+
+    /**
+     * Shows the top five tours for user to view
+     */
+    public static void getTopFiveTours()
+    {
+
+    }
+
+    /**
+     * Takes user back to the home page
+     */
+    public static void goHomeButton()
+    {
+
+    }
 }
