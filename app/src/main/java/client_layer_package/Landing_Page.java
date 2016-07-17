6e722091fe98ee282a53f114457ac0d754fb5f97
SpeedGrapher <andyregueira@gmail.com>
2016-07-17 00:10:17 -0400

    Lots of changes.
    Read the comments.

--- app/src/main/java/client_layer_package/Landing_Page.java
+++ app/src/main/java/client_layer_package/Landing_Page.java
@@ -1,7 +1,42 @@
 package client_layer_package;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.ale.applicaiton_layer_package.R;
+import com.example.ale.applicaiton_layer_package.appController;
+
 /**
- * Created by Ale on 7/10/2016.
+ * Created by Andy
  */
-public class Landing_Page {
+public class Landing_Page extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("YAY!!!!");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing);
+        Button logoutButton = (Button) (findViewById(R.id.logoutButton));
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                appController.disconnect();
+                System.out.println("Succesfully disconnected");
+                logoutRedirect();
+            }
+        });
+
+    }
+    /*
+    Android activities are stored in the activity stack.
+    Since we called this activity (which is LandingPage) using finish function takes you back to the previous activity which is login.
+    If you need to go back to a specific activity then use finishActivity() method.
+     */
+    private void logoutRedirect() {
+        finish();
+    }
 }
+
--- app/src/main/java/database_layer_package/DatabaseRead.java
+++ app/src/main/java/database_layer_package/DatabaseRead.java
@@ -1,7 +1,119 @@
 package database_layer_package;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  * Created by Ale on 6/30/2016.
  */
 public class DatabaseRead {
+
+    databaseConnection db;
+    private String query; //query String. Andy
+    ResultSet resultSet; //for sql result. Andy
+
+    //when databaseRead is instantiated it gets the singleton instance from dbConnection. Andy
+    public DatabaseRead() {
+        db = db.getInstance();
+    }
+    public int getUserId(String username) {
+        query = "select user_id from User where username = " + "'" + username + "'";
+        int id = 0;
+        try {
+            resultSet = db.getCon().createStatement().executeQuery(query);
+            resultSet.next();
+            id = resultSet.getInt("user_id");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return id;
+    }
+    //method to get username from database. Andy
+    public String getPassword(String username)  {
+        query = "select password from User where username = " + "'" + username + "'";
+        String passComparison = "";
+        try {
+            resultSet = db.getCon().createStatement().executeQuery(query);
+            resultSet.next();
+            passComparison = resultSet.getString("password");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return passComparison;
+    }
+    public String getFirstName(String username)  {
+        query = "select first_name from User where username = " + "'" + username + "'";
+        String firstname = "";
+        try {
+            resultSet = db.getCon().createStatement().executeQuery(query);
+            resultSet.next();
+            firstname = resultSet.getString("first_name");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return firstname;
+    }
+    public String getLastName(String username)  {
+        query = "select last_name from User where username = " + "'" + username + "'";
+        String lastname = "";
+        try {
+            resultSet = db.getCon().createStatement().executeQuery(query);
+            resultSet.next();
+            lastname = resultSet.getString("last_name");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lastname;
+    }
+    public String getEmail(String username)  {
+        query = "select last_name from User where username = " + "'" + username + "'";
+        String email = "";
+        try {
+            resultSet = db.getCon().createStatement().executeQuery(query);
+            resultSet.next();
+            email = resultSet.getString("last_name");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return email;
+    }
+    public ArrayList<Object> getTourById(int tourId)  {
+        ArrayList<Object> tour = new ArrayList<>();
+        query = "select * from Tour where tour_id = " + "'" + tourId + "'";
+        try {
+            resultSet = db.getCon().createStatement().executeQuery(query);
+            while(resultSet.next()) {
+                tour.add(resultSet.getObject("name"));
+                tour.add(resultSet.getObject("description"));
+                tour.add(resultSet.getObject("city"));
+                tour.add(resultSet.getObject("state"));
+                tour.add(resultSet.getObject("zipcode"));
+                tour.add(resultSet.getObject("date"));
+                tour.add(resultSet.getObject("address_line1"));
+                tour.add(resultSet.getObject("apartment_number"));
+                tour.add(resultSet.getObject("time"));
+            }
+            //email = resultSet.getString("first_name");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return tour;
+    }
+    public ArrayList<Integer> userFollowTours(int userId)  {
+        ArrayList<Integer> followTours = new ArrayList<>();
+        query = "select * from Signup_Tour where user_id = " + "'" + userId + "'";
+        try {
+            resultSet = db.getCon().createStatement().executeQuery(query);
+            while(resultSet.next()) {
+                followTours.add(resultSet.getInt("tour_id"));
+            }
+            //email = resultSet.getString("first_name");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return followTours;
+    }
 }
