3a18e554bdffd4425878a77a5696871a70f81808
Adrian Bureu <adrianbureu@Adrians-Mac-Pro.local>
2016-07-17 21:33:54 -0400

    completed user class and added get tours method to database read

--- app/src/main/java/database_layer_package/DatabaseRead.java
+++ app/src/main/java/database_layer_package/DatabaseRead.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Created by Ale on 6/30/2016.
@@ -19,6 +20,35 @@ public class DatabaseRead {
     public DatabaseRead() {
         db = db.getInstance();
     }
+
+
+    /**
+     * gets a list of tours (ids) user has created, given username.
+     * @param username
+     * @return array of tour ids corresponding to user
+     */
+    public ArrayList<Integer> getTours(String username)
+    {
+        query = "select tour_id from `User` as u INNER JOIN Create_Tour as c ON u.user_id = c.user_id WHERE username = " + "'" + username + "'";
+        ArrayList<Integer> idArray = new ArrayList<>();
+
+        try {
+        resultSet = db.getCon().createStatement().executeQuery(query);
+        while(resultSet.next()) {
+            idArray.add(resultSet.getInt("tour_id"));
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+        return idArray;
+    }
+
+
+    /**
+     *
+     * @param username
+     * @return the password given the username
+     */
     public int getUserId(String username) {
         query = "select user_id from User where username = " + "'" + username + "'";
         int id = 0;
